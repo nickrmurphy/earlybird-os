7df5d02424528875fd558617fc1c7187b554b39c
nickrmurphy <hi@nickmurphy.io>
2024-08-17 10:22:03 -0500

    init new project structure

--- client/src-tauri/src/main.rs
+++ client/src-tauri/src/main.rs
@@ -1,12 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use earlybird_data::hello;
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    hello();
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
